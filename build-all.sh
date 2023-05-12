e1ca0cb028b2cf772dcf7e72790cc5fcb67f0ebe
Marcel Kornblum <marcel.kornblum@digital.trade.gov.uk>
2023-05-12 10:05:02 +0100

    try with arm64 thrown in

--- build-all.sh
+++ build-all.sh
@@ -74,7 +74,7 @@ build_and_tag () {
     fi
 
     # Build the image
-    docker buildx build --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} --build-arg APT_REPOSITORY=${APT_REPOSITORY} --build-arg PYTHON_VERSION=${PYTHON_VERSION} --build-arg NODE_VERSION=${NODE_VERSION} --platform linux/amd64 ${TAG_ARGS} -f Dockerfile --push .
+    docker buildx build --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} --build-arg APT_REPOSITORY=${APT_REPOSITORY} --build-arg PYTHON_VERSION=${PYTHON_VERSION} --build-arg NODE_VERSION=${NODE_VERSION} --platform linux/amd64, linux/arm64/v8 ${TAG_ARGS} -f Dockerfile --push .
 
     buildend=$(date +%s)
     status=$?
